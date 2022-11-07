03db5a32f259ff9c091414d02e057dd8a7d1b12f
gentambob <44304798+gentambob@users.noreply.github.com>
2022-11-07 11:48:51 +0700

    Update survey.py

--- survey.py
+++ survey.py
@@ -6,7 +6,7 @@ Created on Tue Aug 23 11:25:41 2022
 @author: genta
 """
 
-import geopadas as gpd
+import geopandas as gpd
 import streamlit as st
 from streamlit_folium import folium_static
 import shapely
@@ -48,7 +48,7 @@ def data_all():
     fol=datp.explore("cluster", categorical=True, cmap="prism")
     return data, fol, groups
 data, fol, groups=data()
-st.title("zore grid and unsure survey points")
+st.title("Zore grid and unsure survey points")
 c=st.sidebar.selectbox("cluster (targets)",  list(data["cluster"]))
 cd=groups.get_group(c)
 folc=cd.explore()
@@ -59,76 +59,3 @@ routes=googlerouting (
     [["x", "y"]].sort_values(["x","y"]
      ))
 st.write(routes)
-"""
-def plans_implement(geom):
-    try:
-        plans=tsp(geom, plot=False)
-    except:
-        plans=None
-    if plans is not None:
-        plans=split_dataframe(plans)
-        plans=[p for p in plans if len(p)>0]
-        routes=[]
-        for i, plan in enumerate(plans):
-            routes.append(googlerouting(plan))
-        return routes
-    else:
-        return []
-    
-def tsp_2d(coords_list):
-    fitness = mlrose.TravellingSales(coords = coords_list)
-    #fitness = mlrose.TravellingSales(distances = dist_list)
-    problem_fit = mlrose.TSPOpt(length = len(coords_list), fitness_fn = fitness, maximize=False)
-    best_state, best_fitness = mlrose.genetic_alg(problem_fit, mutation_prob = 0.2,
-                                                  max_attempts =100, random_state = 2)
-    return best_state
-
-
-def percentsample(data, perce):
-    n=(perce/100)*len(data)
-    return data.sample(round(n))
-
-def tsp(geom, plot=False):
-    G=osmnx.graph.graph_from_polygon(geom, retain_all=False,truncate_by_edge =True)
-    tsp = nx.approximation.traveling_salesman_problem
-    nodes, edges = osmnx.graph_to_gdfs(G)
-    nodes=nodes[nodes["street_count"]>1]
-    nodes=percentsample(nodes, 10) # 10% (random) of all nodes within the boundaries with street count >1 as initial state/filtering
-    noid=list(nodes.index) 
-    print(f"{len (noid)} nodes to be visited")
-    if len (noid)==0:
-        noid==None
-    if len(nodes)>1:
-        best_state=tsp(G.to_undirected(), nodes=noid,
-                       weight="length")
-        best_state=list(set(best_state))
-    
-        nodes, edges = osmnx.graph_to_gdfs(G)
-        
-        num=0
-        for b in best_state:
-            nodes.at[b, "state"]=num
-            num=num+1
-        nodes=nodes[nodes["state"].notna()==True]
-    else:
-        nodes["state"]=0
-    if plot==True:
-        n, edges = osmnx.graph_to_gdfs(G)
-        ax=n.plot(color="black", alpha=0.4)
-        edges.plot(ax=ax)
-        nodes.plot(color="red", ax=ax)
-        for idx, row in nodes.iterrows():
-            ax.annotate(text=int(row['state']), xy=[row["x"],row["y"]],
-                         horizontalalignment='center')
-    nodes=nodes.to_crs("EPSG:3857")
-    plan=nodes.sort_values("state")[["x","y"]]
-    return plan
-    
-
-def split_dataframe(df, chunk_size = 5): 
-    chunks = list()
-    num_chunks = len(df) // chunk_size + 1
-    for i in range(num_chunks):
-        chunks.append(df[i*chunk_size:(i+1)*chunk_size])
-    return chunks
-"""

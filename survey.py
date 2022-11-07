4bb6306b5f55000b0e54abc3bc9ea74fd08777ba
gentambob <44304798+gentambob@users.noreply.github.com>
2022-11-07 12:10:10 +0700

    Update survey.py

--- survey.py
+++ survey.py
@@ -51,7 +51,7 @@ data, fol, groups=data_all()
 @st.cache(suppress_st_warning=True,allow_output_mutation=True)
 def cluster_map(c, groups):
     cd=groups.get_group(c)
-    folc=cd.explore("kind", categorical=True, cmap="prism", legend=True)
+    folc=cd.explore("kind", categorical=True, cmap="turbo", legend=True)
     return folc, cd
 
 

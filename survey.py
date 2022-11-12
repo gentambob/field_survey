d34ecdff71451a5466c70681246e3e2bd72da80d
gentambob <44304798+gentambob@users.noreply.github.com>
2022-11-12 16:16:55 +0700

    Update survey.py

--- survey.py
+++ survey.py
@@ -78,15 +78,15 @@ if genre == "cluster":
     cd.to_crs("epsg:900913")
     [["x", "y"]].sort_values(["x","y"]
      ))
-    with st.expander("map", True):
+    with space1.expander("map", True):
             folium_static(folc)
-    with st.expander("routes"):
+    with space1.expander("routes"):
         left, center, right=st.columns([2,2,1])
         for i, v in routes[1].items():
             center.write(f"[link to index {i}]({v})")
         right.write(f"[link all routes]({routes[0]})")
     st.markdown("""-----""")
-    with st.expander("input form"):
+    with space1.expander("input form"):
         form='<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfGxtpiSVJ2hHzMeqb7HikVtzNYy1kRZLlWg1BW_3aQs1xVew/viewform?embedded=true" width="660" height="1500" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>'
         st.markdown(form, unsafe_allow_html=True)
 if genre =="all map":

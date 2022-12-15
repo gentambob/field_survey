dcc6935fa29f62dc4dc16373d73501a5d79a2ada
gentambob <44304798+gentambob@users.noreply.github.com>
2022-12-15 14:06:49 +0700

    Update survey.py

--- survey.py
+++ survey.py
@@ -76,7 +76,7 @@ if genre == "cluster":
     c=st.sidebar.selectbox("cluster (targets)",  sorted(list(data["cluster"].unique())))
     cdplot, cd=cluster_map(c, groups)
     folc=gpd.clip(grid_profile,shapely.geometry.box(* cd.total_bounds))[["geometry", "points"]].explore("points", legend_kwds={"colorbar":False, "interval":True})
-    folc=cdplot.explore("kind", m=folc, categorical=True, cmap="Set1", legend=True)
+    folc=cdplot.explore("kind", m=folc, categorical=True, cmap="Set2", legend=True)
     routes=googlerouting (
     cd.to_crs("epsg:900913")
     [["x", "y"]].sort_values(["x","y"]

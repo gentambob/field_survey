0fac48e3a7347dd9d47ef2221efb12237eb9f54f
gentambob <44304798+gentambob@users.noreply.github.com>
2022-12-31 20:01:31 +0700

    Update survey.py

--- survey.py
+++ survey.py
@@ -79,7 +79,7 @@ def generate_localMap(c):
                     pol=pol.geometry.values[0]
                     geoser=project_gdf(gpd.GeoDataFrame(geometry=gpd.GeoSeries(selection), crs=line_inside.crs))
                     mind=geoser.distance(pol).min()
-                    if mind>6:
+                    if mind>30:
                         st.write(f"[link to ungated strt: {g}]({base})")
 
 
